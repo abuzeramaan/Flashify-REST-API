b84f569a90b0275bf07203c8330008f226cb5738
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2021-11-29 21:54:36 +0530

    added dynamic url generation

--- controllers/satellite.js
+++ controllers/satellite.js
@@ -1,13 +1,15 @@
 const fetch = require("node-fetch");
 
 exports.getSatelliteImages = (req, res, next) => {
-    let latitude = "17.35";
-    let longitude = "78.51";
-    let date = "2018-11-01";
+    let latitude = req.query.latitude;
+    let longitude = req.query.longitude;
+    let date = req.query.date;
+
+    // console.log(req.query);
 
     let url = `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&date=${date}&dim=0.1&api_key=oCCmsqExPBfWPg8gBnjDf22WmPliusk2XPNK7tlO`;
 
-    // console.log(url);
+    console.log(url);
 
     fetch(url)
         .then((response) => {
